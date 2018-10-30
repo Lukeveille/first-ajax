cf0203e2b7324e75bd730075acdf4429be988010
Lukeveille <luke@lukeleveille.com>
2018-10-30 15:36:26 -0400

    completed steps 1 and 2

--- ajax.js
+++ ajax.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var step12Btn = document.querySelector('#step12 button');
 
-  /* Your code goes here */
-
+  step12Btn.addEventListener('click', function() {
+    axios({
+      url: 'http://intro-ajax-api.herokuapp.com/',
+      method: 'get',
+      data: '',
+      dataType: ''
+    })
+  });
 });
