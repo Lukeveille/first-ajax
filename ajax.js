c36ec4179e9540b178ea35e717684b2d86e670d0
Lukeveille <luke@lukeleveille.com>
2018-10-30 16:34:59 -0400

    completed steps 3, 4, 5, and 6, with a custom ping/pong button

--- ajax.js
+++ ajax.js
@@ -1,12 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var step12Btn = document.querySelector('#step12 button');
+  var step12Btn = document.querySelector('#step12 button'),
+  step3456Btn = document.querySelector('#step3456 button'),
+  pingPongBtn = document.querySelector('#pingPongBtn'),
+  pingorpong = 'ping';
+  pingPongData = true;
 
   step12Btn.addEventListener('click', function() {
     axios({
       url: 'http://intro-ajax-api.herokuapp.com/',
       method: 'get',
       data: '',
-      dataType: ''
-    })
+      dataType: 'text'
+    });
   });
+  step3456Btn.addEventListener('click', function() {
+    var section3456 = document.querySelector('#step3456 p');
+    
+    axios({
+      url: 'http://intro-ajax-api.herokuapp.com/' + pingorpong,
+      method: 'get',
+      data: '',
+      dataType: 'text'
+    }).then(function(response) {
+      console.log(response.data);
+      section3456.innerHTML = response.data;
+    }).catch(function() {
+      section3456.innerHTML = "Server failed :'( Will try better next time";
+    }).then(function() {
+      section3456.innerHTML = "It is finished";
+    });
+  });
+
+  pingPongBtn.addEventListener('click', pingPong);
+
+  function pingPong() {
+    if (pingorpong === 'ping') {
+      pingorpong = 'pong';
+    } else {
+      pingorpong = 'ping';
+    }
+    document.querySelector('#pingorpong').innerHTML = pingorpong
+  }
 });
